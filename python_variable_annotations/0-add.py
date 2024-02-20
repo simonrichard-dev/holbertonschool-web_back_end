704e9a9eb6ebbb84cd0302d65760c86ce08a898d
simonrichard-dev <simon.insa@gmail.com>
2024-02-20 11:10:21 +0100

    correct pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,8 +2,8 @@
 """ Basic annotations - add """
 
 
-def add (a: float, b: float) -> float:
-    """  takes a float a and a float b as arguments and returns their sum as a float.
+def add(a: float, b: float) -> float:
+    """ takes floats as arguments and returns their sum as a float.
         Args:
         a: float;
         b: float;
