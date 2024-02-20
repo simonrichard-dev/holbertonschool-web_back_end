4125268c47cd85e61d954c78b83fba8eede58274
simonrichard-dev <simon.insa@gmail.com>
2024-02-20 13:48:50 +0100

    add files for task 1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat """
+
+
+def add(str1: str, str2: str) -> str:
+    """ takes str as arguments & returns sum as a concatened str.
+        Args:
+        str1: str;
+        str2: str;
+    """
+    if type(str1) is str and type(str2) is str:
+        return str1 + str2
+    else:
+        return False
