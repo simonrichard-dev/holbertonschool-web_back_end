194e879c34666cb721a58f0825cd50cd850c27be
simonrichard-dev <simon.insa@gmail.com>
2024-02-20 18:52:46 +0100

    suppress a empty line

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,7 +3,6 @@
 from typing import Iterable, Sequence, List, Tuple
 
 
-
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Function that return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
