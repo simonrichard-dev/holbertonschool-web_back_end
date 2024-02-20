d8247c6fe481094ec9e97d10d5e2b69f7192801a
simonrichard-dev <simon.insa@gmail.com>
2024-02-20 14:10:58 +0100

    update concat function

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,11 +2,11 @@
 """ Basic annotations - concat """
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """ takes str as arguments & returns sum as a concatened str.
         Args:
-        str1: str;
-        str2: str;
+            str1: str;
+            str2: str;
     """
     if type(str1) is str and type(str2) is str:
         return str1 + str2
