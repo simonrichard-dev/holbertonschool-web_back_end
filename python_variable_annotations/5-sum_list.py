c6cdf20372a53fb3f5e10f509a69e2181dcd4048
simonrichard-dev <simon.insa@gmail.com>
2024-02-20 15:55:17 +0100

    update annotation for arg passed to sum_list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Complex types - list of floats """
 import math
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ takes a list of float as arg & returns his sum as float.
         Args:
             input_list: list of float;
