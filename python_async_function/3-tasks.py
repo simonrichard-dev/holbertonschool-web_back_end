3021767875612cd830aee4055c92d4a098fccbf7
simonrichard-dev <simon.insa@gmail.com>
2024-02-21 18:08:35 +0100

    correct everything in the function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+""" 3-Tasks """
+
+
 import asyncio
-from asyncio.tasks import Task
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay: int) -> Task:
-    async def wait_random_wrapper():
-        result = await wait_random(max_delay)
-        return result
 
-    return asyncio.create_task(wait_random_wrapper())
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Function that return a asyncio task
+        Args:
+            max_delay: int;
+        Return:
+            asynchronous task;
+    """
+    return asyncio.create_task(wait_random(max_delay))
