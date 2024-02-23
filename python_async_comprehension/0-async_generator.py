ca26de0a78889b6b5451d34e12326ab4a90ab37d
simonrichard-dev <simon.insa@gmail.com>
2024-02-23 12:02:02 +0100

    add file for task 0. Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Async Generator """
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ corotine that wait a random delay and return it
+        Args:
+            None
+        Return:
+            float;
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
