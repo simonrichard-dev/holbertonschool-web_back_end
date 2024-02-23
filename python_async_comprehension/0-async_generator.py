1f901a8ebee3695f0b4087a7f70ab0abb9a5e885
simonrichard-dev <simon.insa@gmail.com>
2024-02-23 14:12:26 +0100

    return previous version

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ corotine that wait a random delay and return
         a yield generator
         Args:
