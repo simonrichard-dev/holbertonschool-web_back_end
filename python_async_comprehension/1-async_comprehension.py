61132883be0ed062eccc59ac944e8397f423ef36
simonrichard-dev <simon.insa@gmail.com>
2024-02-23 16:18:39 +0100

    add file for task 1. Async Comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""  Async Comprehensions """
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ coroutine will collect 10 random numbers
+        using an async comprehensing
+        Args:
+            None
+        Return:
+            list of float;
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+
+asyncio.run(async_comprehension())
