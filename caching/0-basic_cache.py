77179d553746645c496615168bf2369f07af640a
simonrichard-dev <simon.insa@gmail.com>
2024-02-24 20:52:14 +0100

    correct if condition in put method

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,15 +5,15 @@ from typing import Any, Optional
 BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
-    def put(self, key: Optional[Any], item: Any) -> None:
+    def put(self, key, item):
         """Add an item to the cache
         if both key and item are not None"""
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
         else :
             pass
 
-    def get(self, key: Optional[Any]) -> Any:
+    def get(self, key):
         """Retrieve an item from the cache if
         the key is not None and exists"""
         if key is not None and key in self.cache_data:
