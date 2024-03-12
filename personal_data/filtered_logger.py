e839fb62a1aa6183e788d2e1f1319cafe1df7b4a
simonrichard-dev <simon.insa@gmail.com>
2024-03-12 19:11:06 +0100

    update length because PEP8

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,18 +6,12 @@ Filter Datum
 from typing import List
 import re
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Obfuscates specified fields in a log message.
-
-    Args:
-        fields (List[str]): A list of strings representing fields to obfuscate.
-        redaction (str): A string representing the value by which the fields will be obfuscated.
-        message (str): A string representing the log line.
-        separator (str): A string representing the character separating all fields in the log line.
-
-    Returns:
-        str: The log message with obfuscated fields.
-
-    """
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    """Obfuscates specified fields in a log message."""
     return re.sub(r'(' + '|'.join(fields) + ')=([^' + separator + ']+)',
                   r'\1=' + redaction, message)
