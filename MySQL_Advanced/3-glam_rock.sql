2e0940e1540dc065923744d9c34d9f206a24347c
simonrichard-dev <simon.insa@gmail.com>
2024-03-24 20:49:08 +0100

    add file for task 3. Old school band

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- script that creates a trigger that decreases
+-- the quantity of an item after adding a new order.
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
