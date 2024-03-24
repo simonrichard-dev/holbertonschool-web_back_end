82412c2d7f460ceba092688c29eda980b9ee0daa
simonrichard-dev <simon.insa@gmail.com>
2024-03-24 20:51:43 +0100

    update file to correct red checks

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,7 @@
 -- script that creates a trigger that decreases
 -- the quantity of an item after adding a new order.
 
-CREATE TRIGGER decrease_quantity_after_order
-AFTER INSERT ON orders
-FOR EACH ROW
-UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;
+SELECT  band_name,(ifnull(split,2020) - ifnull(formed,0)) lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
