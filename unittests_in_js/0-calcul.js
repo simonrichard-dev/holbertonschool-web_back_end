6ee6f5a9545fbf9107302f6145525324ff30674e
simonrichard-dev <simon.insa@gmail.com>
2024-03-26 15:52:12 +0100

    add files for task 0. Basic test with Mocha and Node assertion library

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} the sum of a and b
+ */
+
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+  }
+
+module.exports = calculateNumber;
