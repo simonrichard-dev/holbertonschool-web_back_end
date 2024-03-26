621e7124f47453339263e08ab4ec782f6aec6ce3
simonrichard-dev <simon.insa@gmail.com>
2024-03-26 19:47:35 +0100

    update if condition in calculateNumber function

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -12,7 +12,7 @@ function calculateNumber(type, a, b) {
     } else if (type == 'SUBSTRACT') {
       return Math.round(a) - Math.round(b);
     } else if (type == 'DIVIDE') {
-        if (b == 0) {
+        if (Math.round(b) == 0) {
             return 'Error';
         } else {
             return Math.round(a) / Math.round(b);
