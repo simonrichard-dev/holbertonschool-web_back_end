023766c7c6123de8b7c38dae65a9b14eb793d25b
simonrichard-dev <simon.insa@gmail.com>
2024-03-26 17:34:30 +0100

    update for red Check 3 - add a test for negative numbers

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -18,4 +18,20 @@ describe('calculateNumber', function() {
   it('should return 6 when rounding 1.5 and 3.7', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  it('should return 6 when rounding 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 6 when rounding 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return -2 when rounding 1 and -3', () => {
+    assert.equal(calculateNumber(1, -3), -2);
+  });
+
+  it('should return 2 when rounding -1 and 3', () => {
+    assert.equal(calculateNumber(-1, 3), 2);
+  })
 });
