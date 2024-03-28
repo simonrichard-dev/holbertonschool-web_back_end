aa59ea4d0147e1d0eb19459f2979dd2e1987d52a
simonrichard-dev <simon.insa@gmail.com>
2024-03-28 10:23:09 +0100

    refacto tests

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -2,133 +2,80 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function() {
 
+// SUM
 
-    // SUM
+describe('calculateNumber testing SUM', function() {
 
-  it('should return 4 when adding 1 and 3', function() {
-    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-  });
-  
-  it('should return 4 when adding 1 and PI', function() {
-    assert.strictEqual(calculateNumber('SUM', 1, Math.PI), 4);
+  it('returns the good result when add two pos numbers', function() {
+    assert.equal(calculateNumber('SUM', 1, 2.8), 4);
+    assert.equal(calculateNumber('SUM', 1, Math.PI), 4);
   });
 
-  it('should return 5 when adding 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
-  });
-
-  it('should return 5 when adding 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
-  });
-
-  it('should return 6 when adding 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
-  });
-
-  it('Correctly returns an error for invalid input types', () => {
-    assert.throws(() => calculateNumber("a", 2), Error);
-    assert.throws(() => calculateNumber(2, "b"), Error);
-  })
-
-  it('should return -2 when adding 1 and -3', () => {
+  it('returns the good result when add a neg and a pos number', function() {
     assert.equal(calculateNumber('SUM', 1, -3), -2);
-  });
-
-  it('should return 2 when adding -1 and 3', () => {
     assert.equal(calculateNumber('SUM', -1, 3), 2);
+  });
+
+  it('returns the good result add two neg numbers', function() {
+    assert.equal(calculateNumber('SUM', -1, -3.7), -5);
+    assert.equal(calculateNumber('SUM', -1.6, -3.7), -6);
+  });
+
+  it('Correctly returns an error for invalid input types', function() {
+    assert.throws(() => calculateNumber('SUM', "a", 2), Error);
+    assert.throws(() => calculateNumber('SUM', 2, "b"), Error);
   })
-
-  it('should return undefined when missing first argument', () => {
-    assert.equal(calculateNumber('', -1, 3), undefined);
-  })
-
-  it('should return undefined if first argument not correct', () => {
-    assert.equal(calculateNumber('SEUM', -1, 3), undefined);
-  })
+});
 
 
-  // SUBSTRACT
+// SUBSTRACT
 
-  it('should return -2 when substracting 1 and 3', function() {
-    assert.strictEqual(calculateNumber('SUBSTRACT', 1, 3), -2);
+describe('calculateNumber testing SUBSTRACT', function() {
+
+  it('returns the good result when substract two pos numbers', function() {
+    assert.equal(calculateNumber('SUBSTRACT', 1, 3.2), -2);
+    assert.equal(calculateNumber('SUBSTRACT', 6, 4), 2);
   });
 
-  it('should return -2 when substracting 1 and PI', function() {
-    assert.strictEqual(calculateNumber('SUBSTRACT', 1, Math.PI), -2);
-  });
-
-  it('should return -3 when substracting 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber('SUBSTRACT', 1, 3.7), -3);
-  });
-
-  it('should return -3 when substracting 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber('SUBSTRACT', 1.2, 3.7), -3);
-  });
-
-  it('Correctly returns an error for invalid input types', () => {
-    assert.throws(() => calculateNumber("a", 2), Error);
-    assert.throws(() => calculateNumber(2, "b"), Error);
-  })
-
-  it('should return -2 when substracting 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber('SUBSTRACT', 1.5, 3.7), -2);
-  });
-
-  it('should return 4 when substracting 1 and -3', () => {
+  it('returns the good result when substract a neg and a pos number', function() {
     assert.equal(calculateNumber('SUBSTRACT', 1, -3), 4);
-  });
-
-  it('should return -4 when substracting -1 and 3', () => {
     assert.equal(calculateNumber('SUBSTRACT', -1, 3), -4);
+  });
+
+  it('returns the good result substract two neg numbers', function() {
+    assert.equal(calculateNumber('SUBSTRACT', -1, -3.7), 3);
+    assert.equal(calculateNumber('SUBSTRACT', -10, -4), -6);
+  });
+
+  it('Correctly returns an error for invalid input types', function() {
+    assert.throws(() => calculateNumber('SUBSTRACT', "a", 2), Error);
+    assert.throws(() => calculateNumber('SUBSTRACT', 2, "b"), Error);
   })
+});
 
 
-  // DIVIDE
+// DIVIDE
 
-  it('should return 4 when divide 1 and 3', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 3), 0.3333333333333333);
+describe('calculateNumber testing DIVIDE', function() {
+
+  it('returns the good result dividing two pos numbers', function() {
+    assert.equal(calculateNumber('DIVIDE', 6, 2), 3);
+    assert.equal(calculateNumber('DIVIDE', Math.PI, 1), 3);
   });
 
-  it('should return 4 when divide 1 and PI', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1, Math.PI), 0.3333333333333333);
+  it('returns the good result dividing a neg and a pos number', () => {
+    assert.equal(calculateNumber('DIVIDE', -4, 2.2), -2);
+    assert.equal(calculateNumber('DIVIDE', -1.6, 3.7), -0.5);
   });
 
-  it('should return 5 when divide 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.25);
+  it('returns the good result dividing two neg numbers', function() {
+    assert.equal(calculateNumber('DIVIDE', -4, -2.2), 2);
+    assert.equal(calculateNumber('DIVIDE', -1.6, -3.7), 0.5);
   });
 
-  it('Correctly returns an error for invalid input types', () => {
-    assert.throws(() => calculateNumber("a", 2), Error);
-    assert.throws(() => calculateNumber(2, "b"), Error);
-  })
-
-  it('should return 5 when divide 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.25);
-  });
-
-  it('should return 6 when divide 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
-  });
-
-  it('should return -2 when divide 1 and -3', () => {
-    assert.equal(calculateNumber('DIVIDE', 1, -3), -0.3333333333333333);
-  });
-
-  it('should return 2 when divide -1 and 3', () => {
-    assert.equal(calculateNumber('DIVIDE', -1, 3), -0.3333333333333333);
-  })
-
-  it('should return -Infinity when divide -1 and 0', () => {
-    assert.equal(calculateNumber('DIVIDE', -1, 0), 'Error');
-  })
-
-  it('should return Infinity when divide 1 and 0', () => {
-    assert.equal(calculateNumber('DIVIDE', 1, 0), 'Error');
-  })
-
-  it('should return Error when divide 0 and 0', () => {
-    assert.equal(calculateNumber('DIVIDE', 0, 0), 'Error');
+  it('Correctly returns an error for invalid input types', function() {
+    assert.throws(() => calculateNumber('DIVIDE', "a", 2), Error);
+    assert.throws(() => calculateNumber('DIVIDE', 2, "b"), Error);
   })
 });
