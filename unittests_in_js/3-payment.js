db291d6e0822d1f19d5203f010d2018d390b8e9b
simonrichard-dev <simon.insa@gmail.com>
2024-03-28 09:20:56 +0100

    update import name

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,7 +1,7 @@
-const Utilis = require('./utils');
+const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const total = Utilis.calculateNumber('SUM', totalAmount, totalShipping)
+    const total = Utils.calculateNumber('SUM', totalAmount, totalShipping)
     console.log(`The total is: ${total}`);
 }
 
